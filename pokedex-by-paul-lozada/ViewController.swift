9b253b2a396f908e4eb192d911ce5ab2b5330bf2
PaulLozada <paul_lozada@icloud.com>
2016-02-28 19:25:33 -0700

    Initial Commit

---
+++ pokedex-by-paul-lozada/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-paul-lozada
+//
+//  Created by Paul Lozada on 2016-02-28.
+//  Copyright © 2016 Paul Lozada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ pokedex-by-paul-lozadaTests/pokedex_by_paul_lozadaTests.swift
@@ -0,0 +1,36 @@
+//
+//  pokedex_by_paul_lozadaTests.swift
+//  pokedex-by-paul-lozadaTests
+//
+//  Created by Paul Lozada on 2016-02-28.
+//  Copyright © 2016 Paul Lozada. All rights reserved.
+//
+
+import XCTest
+@testable import pokedex_by_paul_lozada
+
+class pokedex_by_paul_lozadaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
